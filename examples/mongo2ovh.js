2f25a9d8838ac81882104fe30f98245936ea7af7
Scott Frees <scott.frees@gmail.com>
2017-12-22 10:34:28 -0500

    ovh purge and local file cleanup

--- examples/mongo2ovh.js
+++ examples/mongo2ovh.js
@@ -4,6 +4,6 @@ const backup = require('../index');
 
 const backup_service = new backup.Service(backup.MongoSource, backup.OVHStore, backup.EmailReporter);
 
-//backup_service.start();
+backup_service.start();
 
-backup_service.do_backup();
+//backup_service.do_backup();
